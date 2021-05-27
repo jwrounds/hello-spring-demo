6d99419d40cbd343c63cb3604bd918c4b6085356
Jason <jwrounds@usfca.edu>
2021-05-27 15:02:44 -0500

    Added language options to dynamic form greeting, and helper method createMessage

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -9,14 +9,25 @@ import org.springframework.web.bind.annotation.*;
 public class HelloController {
 
 
+    public static String createMessage(String name, String language) {
+        String greeting = switch (language) {
+            case "french" -> "Bonjour, ";
+            case "spanish" -> "Hola, ";
+            case "german" -> "Hallo, ";
+            case "italian" -> "Ciao, ";
+            default -> "Hello, ";
+        };
+        return greeting + name + "!";
+    }
+
     @GetMapping("goodbye")
     public String goodbye() {
         return "Goodbye, Spring!";
     }
-    
+
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, @RequestParam String language) {
+        return createMessage(name, language);
     }
 
     @GetMapping("{name}")
@@ -30,6 +41,13 @@ public class HelloController {
                 "<body>" +
                 "<form action = '/hello' method = 'post'>" + // submit a request to /hello
                 "<input type = 'text' name = 'name' >" +
+                "<select name = 'language' >" +
+                "<option value='french'>French</option>" +
+                "<option value='english'>English</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='german'>German</option>" +
+                "<option value='italian'>Italian</option>" +
+                "</select>" +
                 "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
